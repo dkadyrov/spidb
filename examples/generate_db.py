1ce62e59a90ab0c2a0d7d3844eb3bcda41cd5dca
daniel kadyrov <daniel.kadyrov@gmail.com>
2024-05-29 12:49:26 -0400

    updated examples

--- examples/generate_db.py
+++ examples/generate_db.py
@@ -1,18 +1,24 @@
-# %%
+"""Generate Database
+
+This script demonstrates how to generate a database from the acoustic files and log files. It illustrates how to create a database, add files and logs to the database, and commit the changes.
+"""
+
 from spidb import spidb
 from dankpy import file, dt
 import glob
 import pandas as pd
 
-# %%
+
 # Location of database. It will be created if it does not exist.
 db = spidb.Database(r"data/spi.db")
-# %%
+
 # Location of the acoustic files
 acoustic_files = glob.glob(r"data/aspids/**/*.wav", recursive=True)
 
+# Get the metadata of the acoustic files
 files = file.metadatas(acoustic_files, extended=True, stevens=True)
-#%%
+
+# Add the files to the database
 for f, fi in files.iterrows():
     ff = spidb.File(
         filepath=fi.filepath,
@@ -29,16 +35,22 @@ for f, fi in files.iterrows():
 
     db.session.add(ff)
 db.session.commit()
-#%%
+
 # Location of the ASPID Log File
 log = pd.read_csv(r"data/aspids_log.csv")
 
+# Convert the start and end columns to datetime
 log["start"] = pd.to_datetime(log["start"])
 log["end"] = pd.to_datetime(log["end"])
+
+# Calculate the duration of the logs
 log["duration"] = log["end"] - log["start"]
+
+# Strip the leading and trailing whitespaces from the material and target columns
 log.material = log.material.str.strip()
 log.target = log.target.str.strip()
 
+# Add the logs to the database
 for l, row in log.iterrows():
     ll = spidb.Log(
         start=row["start"],
@@ -53,12 +65,13 @@ for l, row in log.iterrows():
     db.session.add(ll)
 db.session.commit()
 
-#%%
 # Location of the microwave files
 mspids_files = glob.glob(r"data/mspids/**/*.wav", recursive=True)
 
+# Get the metadata of the microwave files
 files = file.metadatas(mspids_files, extended=True, stevens=True)
 
+# Add the files to the database
 for f, fi in files.iterrows():
     ff = spidb.File(
         filepath=fi.filepath,
@@ -76,14 +89,21 @@ for f, fi in files.iterrows():
     db.session.add(ff)
 db.session.commit()
 
-#%%
 # Location of the MSPID Log File
 log = pd.read_csv(r"data/mspids_log.csv")
 
+# Convert the start and end columns to datetime
 log["start"] = pd.to_datetime(log["start"])
 log["end"] = pd.to_datetime(log["end"])
+
+# Calculate the duration of the logs
 log["duration"] = log["end"] - log["start"]
 
+# Strip the leading and trailing whitespaces from the material and target columns
+log.material = log.material.str.strip()
+log.target = log.target.str.strip()
+
+# Add the logs to the database
 for l, row in log.iterrows():
     ll = spidb.Log(
         start=row["start"],
@@ -97,5 +117,3 @@ for l, row in log.iterrows():
     )
     db.session.add(ll)
 db.session.commit()
-
-# %%
--- examples/visualization.py
+++ examples/visualization.py
@@ -1,4 +1,7 @@
-# %%
+""" Visualization 
+
+This script provides examples of how to visualize the data stored in the SPIDB database. The script demonstrates how to display the waveform, spectrogram, and spectra of the audio and microwave sensors. The script also provides examples of how to extract audio data from the database and display the waveform and envelope of the audio signal.
+"""
 from spidb import spidb, visualizer
 from dankpy import file, dt, colors
 import pandas as pd
@@ -6,27 +9,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-plt.style.use("dankpy.styles.neurips")
-# %%
+
 # Initialize the Database
 db = spidb.Database(r"data/spi.db")
 
 # Find the logs with the specific target and material
-logs = db.session.query(spidb.Log).filter(spidb.Log.sensor == "ASPIDS").filter(spidb.Log.target == "Tribolium confusum").filter(spidb.Log.material == "Flour").all()
+logs = (
+    db.session.query(spidb.Log)
+    .filter(spidb.Log.sensor == "ASPIDS")
+    .filter(spidb.Log.target == "Tribolium confusum")
+    .filter(spidb.Log.material == "Flour")
+    .all()
+)
 
 # Get the last log
 log = logs[-1]
 
 # Find a good time period to display the data
-start = log.start + dt.timedelta(seconds=6.75*60)
+start = log.start + dt.timedelta(seconds=6.75 * 60)
 end = start + dt.timedelta(seconds=60)
-#%%
+
 # Plot the Spectra of the Audio Sensor
 fig, ax = plt.subplots()
 for c in [0, 1, 2, 3]:
     audio = db.get_audio(start=start, end=end, sensor="ASPIDS", channel=c)
-    f, p = signal.welch(audio.data.signal, fs=audio.sample_rate, nperseg=1024, scaling="spectrum", window="blackmanharris")
-    ax.plot(f, 10*np.log10(p), label=f"Ch. {c}", c=colors[c])
+    f, p = signal.welch(
+        audio.data.signal,
+        fs=audio.sample_rate,
+        nperseg=1024,
+        scaling="spectrum",
+        window="blackmanharris",
+    )
+    ax.plot(f, 10 * np.log10(p), label=f"Ch. {c}", c=colors[c])
 ax.legend(loc="upper right", ncols=4)
 ax.set_xlim(0, 12000)
 ax.set_ylim(-125, -25)
@@ -34,14 +48,22 @@ ax.set_xlabel("Frequency [Hz]")
 ax.set_ylabel("Magnitude [dB]")
 ax.set_yticks([-125, -75, -25])
 
-#%%
-# Spectrogram Display of the Audio Sensor
 
-fig, axs = visualizer.spectrogram_display(db, start=start, end=end, time_format="seconds", section="all", showscale="True", sensor="ASPIDS", compressed=True)
+# Spectrogram Display of the Audio Sensor
+fig, axs = visualizer.spectrogram_display(
+    db,
+    start=start,
+    end=end,
+    time_format="seconds",
+    section="all",
+    showscale="True",
+    sensor="ASPIDS",
+    compressed=True,
+)
 for ax in axs:
     ax.set_ylim(0, 8000)
     ax.set_yticks([0, 4000, 8000])
-#%%
+
 # Example of extracting audio
 audio = db.get_audio(start=start, end=end, sensor="ASPIDS", channel=0)
 
@@ -52,69 +74,95 @@ audio.plot_waveform(time_format="seconds", ax=ax[0])
 ax[0].set_xlim(0, 60)
 ax[0].set_xlabel("Time [s] \n a")
 
+# Filter the audio signal
 audio.bandpass_filter(500, 6000, overwrite=True)
+
+# Envelope the audio signal
 audio.envelope(overwrite=True)
 
-# fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 ax[1].plot(audio.data["time [s]"], audio.data.signal)
 ax[1].set_xlim(0, 60)
 ax[1].set_ylim(0, 0.02)
 ax[1].set_xlabel("Time [s] \n b")
-# fig.supylabel("Amplitude")
-# fig.supxlabel("Time [s]")
-# #%%
+
+# Example of extracting audio
 log = db.session.get(spidb.Log, 152)
 
-# start = log.start
+start = log.start
 end = start + dt.timedelta(seconds=60)
 audio = db.get_audio(start=start, end=end, sensor="ASPIDS", channel=7)
 
 fig, ax = audio.plot_spectrogram(time_format="seconds", zmin=-140, zmax=-80)
 
-# #%%
 # Example of a Insect in Microwave Sensor
 
 log = db.session.get(spidb.Log, 168)
-start = log.start + dt.timedelta(seconds=4*60)
+start = log.start + dt.timedelta(seconds=4 * 60)
 end = start + dt.timedelta(seconds=60)
 
-# Waveform Display of the Microwave Sensor
 fig, ax = visualizer.waveform_display(db, start=start, end=end, sensor="MSPIDS", time_format="seconds", compressed=True, normalize=True, filter=True, envelope=True)
-#%%
-# Spectrograms of the Microwave Sensor 
-fig, ax = visualizer.spectrogram_display(db, start=start, end=end, sensor="MSPIDS", time_format="seconds", section="all", showscale="True", compressed=True, zmin=-100, zmax=-50)
 
-#%%
+fig, ax = visualizer.spectrogram_display(
+    db, start=start, end=end, sensor="MSPIDS", time_format="seconds", compressed=True
+)
 
+# Waveform Display of the Microwave Sensor
+fig, ax = visualizer.waveform_display(
+    db,
+    start=start,
+    end=end,
+    sensor="MSPIDS",
+    time_format="seconds",
+    compressed=True,
+    normalize=True,
+    filter=True,
+    envelope=True,
+)
 
-#%%
+# Spectrograms of the Microwave Sensor
+fig, ax = visualizer.spectrogram_display(
+    db,
+    start=start,
+    end=end,
+    sensor="MSPIDS",
+    time_format="seconds",
+    section="all",
+    showscale="True",
+    compressed=True,
+    zmin=-100,
+    zmax=-50,
+)
+
 # Spectrogram of the Microwave Sensor with a specific channel
 microwave = db.get_audio(start=start, end=end, sensor="MSPIDS", channel=4)
 fig, ax = microwave.plot_spectrogram(zmin=-100, zmax=-50, time_format="seconds")
-# fig.savefig(rf"reports/documents/NeurIPS_SPID/images/microwave_spectrogram_{log.target}_{log.material}_{c}.pdf", dpi=300, bbox_inches="tight")
 ax.set_ylim(0, 200)
-
-#%%
 
 microwave.lowpass_filter(200, overwrite=True)
 
 microwave.envelope(overwrite=True)
-microwave.data.signal = microwave.data.signal / 0.1*microwave.data.signal.max()
+microwave.data.signal = microwave.data.signal / 0.1 * microwave.data.signal.max()
 
 fig, ax = plt.subplots()
 ax.plot(microwave.data["time [s]"], microwave.data.signal)
 ax.set_xlim(0, 60)
 ax.set_ylim(0, 20)
 
-
-# %%
 # Example of a Noisy Microwave Moment
-
-log = db.session.get(spidb.Log, 239)
+log = db.session.get(spidb.Log, 240)
 start = log.start + dt.timedelta(seconds=0)
 end = start + dt.timedelta(seconds=60)
 
-fig, ax = visualizer.spectrogram_display(db, start=start, end=end, sensor="MSPIDS", time_format="seconds", section="all", showscale="True", compressed=True, zmin=-100, zmax=-50)
-
-# fig.savefig(rf"reports/documents/NeurIPS_SPID/images/microwave_spectrogram_display_{log.target}_{log.material}.pdf", dpi=300, bbox_inches="tight")
-#%%
+fig, ax = visualizer.spectrogram_display(
+    db,
+    start,
+    end,
+    sensor="MSPIDS",
+    section="all",
+    time_format="seconds",
+    zmin=-100,
+    zmax=-50,
+)
+ax[-1].set_ylim(0, 2000)
+ax[-2].set_ylim(0, 2000)
